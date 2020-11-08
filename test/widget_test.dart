1b287bb0b2903e46ddca61632d0645f9633daadb
Eeshan Dutta <53020601+EeshanDutta007@users.noreply.github.com>
2020-11-08 19:14:25 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:Elixir/main.dart';
-
+//basic widget dart
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
